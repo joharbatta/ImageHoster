918e95be3b588965c85c41ea1518cbcf53c58c69
joharbatta <joharbatta@gmail.com>
2020-02-29 21:26:11 +0530

    display image

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -1,16 +1,25 @@
 package ImageHoster.controller;
 
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController{
 
+    @Autowired
+    private ImageService imageService;
     @RequestMapping("/")
-    public String Home()
+    public String Home(Model model)
     {
+        List<Image> images = imageService.getAllImages();
+        model.addAttribute("images1", images);
         return "index";
-
     }
 }
